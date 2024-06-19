cb30b9f9a82605f8d47bc29949faaaa2fafc9540
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-06-19 18:57:03 +0300

    Angular Eğitimi #4 - Data Binding Mekanizması Nedir? Hangi Yaklaşımlarla Gerçekleştirilmektedir?

--- proje1/src/app/app.component.ts
+++ proje1/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    //templateUrl: './app.component.html',
+    template: `<p>merhaba</p>
+  <hr>
+  <div style="background-color: aqua;">
+  <app-home></app-home>
+  </div>`,
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'proje1';
