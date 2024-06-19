82dba5eb94a5dfe408daab2b3c4b0032ca252f4f
Şevval İclal Cebecioğlu <161520054+sevvaliclalcebecioglu@users.noreply.github.com>
2024-06-19 23:30:34 +0300

    Data Binding Mekanizması Nedir? Hangi Yaklaşımlarla Gerçekleştirilmektedir?

--- proje1/src/app/app.component.ts
+++ proje1/src/app/app.component.ts
@@ -6,14 +6,22 @@ import { HomeComponent } from "./home/home.component";
     selector: 'app-root',
     standalone: true,
     //templateUrl: './app.component.html',
-    template: `<p>merhaba</p>
-  <hr>
-  <div style="background-color: aqua;">
-  <app-home></app-home>
-  </div>`,
+    template: `
+   <!-- <h1>{{title}}</h1> => Text Interpolations-->
+
+   //Property Binding
+   <!--<input type="text" [value]="title">
+   <app-home [pageName]="title"></app-home>-->
+   <!--Kendi yaptığımız field'ıda bu şekilde kullanabilirim-->
+
+   //Two-Way Data Binding
+   <!--<input type="text" [(ngModel)]="name">-->
+
+    `,
     styleUrl: './app.component.scss',
     imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'proje1';
+  name: string;
 }
--- proje1/src/app/home/home.component.ts
+++ proje1/src/app/home/home.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html',
+  //templateUrl: './home.component.html',
+  template: `
+        <h1>{{pageName}}</h1>
+  `,
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  @Input() pageName: string; // @ınput yaptığım için dışardan değer alınabilir hale getirdim.
 }
